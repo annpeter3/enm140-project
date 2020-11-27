e6a9441bdac96061e346dce924d4b20e7a1b0c19
Anna Petersson <annpeter@student.chalmers.se>
2020-11-27 15:20:40 +0100

    Changed to numpy arrays

--- campaign.py
+++ campaign.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # The ammount of effort each campaign can allocate in a week.
 effortPerWeek = 3
 # The number of Allocation phases until the winner is declared.
@@ -6,24 +8,26 @@ numberOfWeeks = 5
 numberOfDistricts = 10
 # This variable counts the number of votes that player 1 has in each districts minus the votes needed for a draw in a district.
 # Hence a positive number means that player one wins the district
-districts = [5, 4, 3, 2, 1, -1, -2, -3, -4, -5] # This is could be a set of states where voter support is to 10-0, 9-1, 8-2, 7-3, 6-4, 4-6, 3-7.. etc (but we dont have to care about the absolute number of voters)
-
+districts = np.array([5, 4, 3, 2, 1, -1, -2, -3, -4, -5]) # This is could be a set of states where voter support is to 10-0, 9-1, 8-2, 7-3, 6-4, 4-6, 3-7.. etc (but we dont have to care about the absolute number of voters)
 
 def apply_strategy(districts, week):
     p1effort = player1_allocate(districts)
     p2effort = player2_allocate(districts)
-    for district in range(len(districts)):
-        districts[district] += (p1effort[district] + p2effort[district])
+
+    districts = districts + p1effort + p2effort
+
     print("Polls after week ", week + 1)
     print(districts)
 
+    return districts
+
 # This strategy puts the effort in the states where the player is losing by the smallest margin. (FLAW: if multiple states are equal, it will choose the first index encountered)
 def strat_x(polls):
-    allocation = [0] * numberOfDistricts
+    allocation = np.zeros(numberOfDistricts)
     pollsAfter = polls.copy()
 
-    for x in range(effortPerWeek):
-        minDifNotWinning = pollsAfter.index(max([n for n in pollsAfter if n <= 0]))
+    for _ in range(effortPerWeek):
+        minDifNotWinning = np.argmax(np.nonzero(pollsAfter))
         allocation[minDifNotWinning] += 1
         pollsAfter[minDifNotWinning] += 1
 
@@ -31,14 +35,11 @@ def strat_x(polls):
 
 # This strategy puts the effort in the states where the difference is the smalles. (FLAW: if multiple states are equally distant, it will choose the first index encountered)
 def strat_y(polls):
-    allocation = [0] * numberOfDistricts
+    allocation = np.zeros(numberOfDistricts)
     pollsAfter = polls.copy()
 
-    for x in range(effortPerWeek):
-        for i in range(len(pollsAfter)):
-            if (pollsAfter[i] < 0):
-                pollsAfter[i] = pollsAfter[i] * -1
-        minDif = pollsAfter.index(min([n for n in pollsAfter]))
+    for _ in range(effortPerWeek):
+        minDif = np.argmin(np.abs(pollsAfter))
         allocation[minDif] += 1
         pollsAfter[minDif] += 1
 
@@ -46,14 +47,11 @@ def strat_y(polls):
 
 # Use to test smth vs a player that does nothing.
 def strat_zero(polls):
-    return [0] * numberOfDistricts
+    return np.zeros(numberOfDistricts)
 
 # Multiply all values in a list by -1
 def flip_list(list): 
-    result = [0] * len(list)
-    for i in range(len(list)):
-        result[i] = list[i] * -1
-    return result
+    return (-1)*list
 
 # Runs a strategy for player 1
 def player1_allocate(polls):
@@ -65,18 +63,14 @@ def player1_allocate(polls):
 # Then also flips the allocation before returning since player 2 wants negative numbers
 def player2_allocate(polls):
     p2pov = flip_list(polls)
-    p2allocation = strat_y(polls)
+    p2allocation = strat_y(p2pov)
     return flip_list(p2allocation)
 
-# P1 wins in states with a positive number 
+# P1 wins in states with a positive number
 def declare_winner(polls):
-    p1score = 0
-    p2score = 0
-    for district in range(len(polls)):
-        if polls[district] > 0:
-            p1score += 1
-        elif polls[district] < 0:
-            p2score += 1
+    p1score = len(polls[polls > 0])
+    p2score = len(polls[polls < 0])
+
     if p1score == p2score:
         print("DRAW")
     elif p1score > p2score:
@@ -88,6 +82,6 @@ print("Initial polls:")
 print(districts)
 # Run campaign
 for x in range(numberOfWeeks):
-    apply_strategy(districts, x)
+    districts = apply_strategy(districts, x)
 # Hold election
 declare_winner(districts)
