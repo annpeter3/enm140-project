bc893d805d8d9ec5d2b3ceffaf9f8b4f78392c66
Anna Petersson <annpeter@student.chalmers.se>
2020-12-08 17:01:24 +0100

    Fix for when running several campaigns after each other

--- run.py
+++ run.py
@@ -13,11 +13,11 @@ number_of_weeks = 5             # The number of Allocation phases until the winn
 number_of_districts = 10        # The number of voting districts
 T = 2                           # The number of times the simulation is repeated
 
-districts = np.array([5, 4, 3, 2, 1, -1, -2, -3, -4, -5])   # This variable counts the number of votes that player 1 has in each district 
-                                                            # minus the votes needed for a draw in a district.
-                                                            # Hence a positive number means that player one wins the district
-                                                            # This is could be a set of states where voter support is to 10-0, 9-1, 8-2, 7-3, 6-4, 4-6, 3-7.. etc 
-                                                            # (but we dont have to care about the absolute number of voters)
+initial_districts = np.array([5, 4, 3, 2, 1, -1, -2, -3, -4, -5])   # This variable counts the number of votes that player 1 has in each district 
+                                                                    # minus the votes needed for a draw in a district.
+                                                                    # Hence a positive number means that player one wins the district
+                                                                    # This is could be a set of states where voter support is to 10-0, 9-1, 8-2, 7-3, 6-4, 4-6, 3-7.. etc 
+                                                                    # (but we dont have to care about the absolute number of voters)
 
 # Load all available strategies:
 strategies_dict = strategies.strategies_as_dict()
@@ -25,6 +25,8 @@ strategies_dict = strategies.strategies_as_dict()
 # Run T election campaigns:
 results = np.zeros(T)
 for i in range(T):
+
+    districts = initial_districts
     logging.debug("Initial polls: %s", districts)
     
     # Run campaign for strat_x and strat_y:
