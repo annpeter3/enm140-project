66d8a101ab41fd02e12c90c3dcd7a279e0b14b33
Anna Petersson <annpeter@student.chalmers.se>
2020-12-10 14:28:29 +0100

    Results from parameter sweep added

--- run_parameter_sweep.py
+++ run_parameter_sweep.py
@@ -11,10 +11,10 @@ from run_all_vs_all import process_results
 logging.basicConfig(level=logging.INFO, format='%(message)s') # change between logging.DEBUG and logging.INFO for messages or not
 
 ######### Parameters ######################
-effort_per_week_array = np.arange(start=1, stop=10)            # The amount of effort each campaign can allocate in a week.
-number_of_districts_array = np.arange(start=1, stop=10)        # The number of voting districts
+effort_per_week_array = np.arange(start=1, stop=20)            # The amount of effort each campaign can allocate in a week.
+number_of_districts_array = np.arange(start=1, stop=20)        # The number of voting districts
 number_of_weeks = 10                                           # The number of Allocation phases until the winner is declared.
-T = 1                                                          # The number of times the simulation is repeated
+T = 20                                                          # The number of times the simulation is repeated
 
 ######### Main loop ######################
 # Load all available strategies:
@@ -35,6 +35,8 @@ for effort_per_week in effort_per_week_array:
 
         j += 1
     
+    logging.info("Iteration %d of %d finished", i, len(effort_per_week_array))
+
     i += 1
 
 # Save the result:
@@ -43,28 +45,84 @@ pickle.dump(mean_payoffs, file)
 file.close()
 
 # To access the results, do:
-#file = open('parameter-sweep-results.txt', 'rb')
-#mean_payoffs_2 = pickle.load(file)
+#file = open('./Results/parameter-sweep-results.txt', 'rb')
+#mean_payoffs = pickle.load(file)
 
 ######### Visualise ######################
-fig = plt.figure(figsize=plt.figaspect(0.5))
-
 X,Y = np.meshgrid(effort_per_week_array, number_of_districts_array)
 
-ax = fig.add_subplot(1, 2, 1, projection='3d')
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 surf1 = ax.plot_surface(X, Y, mean_payoffs[:,:,0], cmap='hot')
-#fig.colorbar(surf1, shrink=0.5, aspect=5)
+fig.colorbar(surf1, shrink=0.5, aspect=5)
 
 ax.set_xlabel('Effort per week')
 ax.set_ylabel('Number of districts')
 ax.set_zlabel('Mean payoff strat_x')
+plt.show(block=False)
 
-ax = fig.add_subplot(1, 2, 2, projection='3d')
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 surf2 = ax.plot_surface(X, Y, mean_payoffs[:,:,1], cmap='hot')
-#fig.colorbar(surf2, shrink=0.5, aspect=5)
+fig.colorbar(surf2, shrink=0.5, aspect=5)
 
 ax.set_xlabel('Effort per week')
 ax.set_ylabel('Number of districts')
 ax.set_zlabel('Mean payoff strat_y')
+
+plt.show(block=False)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, mean_payoffs[:,:,2], cmap='hot')
+fig.colorbar(surf2, shrink=0.5, aspect=5)
+
+ax.set_xlabel('Effort per week')
+ax.set_ylabel('Number of districts')
+ax.set_zlabel('Mean payoff strat_zero')
+
+plt.show(block=False)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, mean_payoffs[:,:,3], cmap='hot')
+fig.colorbar(surf2, shrink=0.5, aspect=5)
+
+ax.set_xlabel('Effort per week')
+ax.set_ylabel('Number of districts')
+ax.set_zlabel('Mean payoff strat_random_1')
+
+plt.show(block=False)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, mean_payoffs[:,:,4], cmap='hot')
+fig.colorbar(surf2, shrink=0.5, aspect=5)
+
+ax.set_xlabel('Effort per week')
+ax.set_ylabel('Number of districts')
+ax.set_zlabel('Mean payoff strat_random_2')
+
+plt.show(block=False)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, mean_payoffs[:,:,5], cmap='hot')
+fig.colorbar(surf2, shrink=0.5, aspect=5)
+
+ax.set_xlabel('Effort per week')
+ax.set_ylabel('Number of districts')
+ax.set_zlabel('Mean payoff strat_larger_margin')
+
+plt.show(block=False)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf2 = ax.plot_surface(X, Y, mean_payoffs[:,:,6], cmap='hot')
+fig.colorbar(surf2, shrink=0.5, aspect=5)
+
+ax.set_xlabel('Effort per week')
+ax.set_ylabel('Number of districts')
+ax.set_zlabel('Mean payoff strat_mixed')
 
 plt.show()
