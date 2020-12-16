248d1d62f0fa5e9caa27b6d8bfc09634cb05acd5
Hampus Broman <namorben@gmail.com>
2020-12-16 15:20:03 +0100

    adds districtbuilder uneven district number support

--- districtbuilder.py
+++ districtbuilder.py
@@ -2,13 +2,11 @@ import numpy as np
 import math
 
 def build(number_of_districts, shape, amplitude):
-    if (int(number_of_districts)%2 != 0):
-        raise ValueError("Number of districts must be even")
     districts = np.zeros(number_of_districts, dtype=int)
     length = int(number_of_districts)
     if shape == "stair" :
         for i in range(length):
-            if i < length/2:
+            if i < int(length/2):
                 districts[i] = amplitude * length/2 - i * amplitude
             else:
                 districts[i] = - districts[- (i + 1)]
@@ -16,13 +14,15 @@ def build(number_of_districts, shape, amplitude):
         return np.zeros(number_of_districts)
     elif shape == "polarized":
         for i in range(length):
-            if i >= length/2:
+            if i == int(length/2) and (i + 1) * 2 > length:
+                districts[i] = 0
+            elif i >= length/2:
                 districts[i] = -amplitude
             else:
                 districts[i] = amplitude
     elif shape == "cosine":
         for i in range(length):
-            if i < length/2:
+            if i < int(length/2):
                 districts[i] = int(amplitude * math.cos(i * 1.57 / (length / 2 -1)))
             else:
                 districts[i] = - districts[- (i + 1)]
